3366993a307c26158e035883e0a7fe2802d7f43d
Rostislav <klepikovrostk@gmail.com>
2017-12-19 16:25:24 +0200

    first commit

---
+++ src/main/java/MyMaven/maven/App.java
@@ -0,0 +1,13 @@
+package MyMaven.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
